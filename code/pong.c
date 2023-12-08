5a96d92f9b8455760366a8efc3cf8631d5c80c33
dslbit <dsilvapersonalemail@gmail.com>
2023-12-08 10:25:41 -0400

    Rectangle colors goes float

--- code/pong.c
+++ code/pong.c
@@ -17,11 +17,19 @@ INTERNAL S32 round_f32_to_s32(F32 value) {
   return result;
 }
 
-/* TODO: Move to 'renderer' */
-EXTERNIZE void draw_rect(GameBackBuffer *back_buffer, F32 x, F32 y, F32 width, F32 height, U32 color) {
+INTERNAL U32 round_f32_to_u32(F32 value) {
+  U32 result;
+  
+  result = CAST(U32) (value + 0.5f);
+  return result;
+}
+
+/* TODO: Move to 'renderer' ; color struct ; make_rgb_to_float/make_hex_to_float? */
+EXTERNIZE void draw_rect(GameBackBuffer *back_buffer, F32 x, F32 y, F32 width, F32 height, F32 R, F32 G, F32 B) {
   S32 i, j;
   S32 start_x, start_y, end_x, end_y;
   U32 *pixel;
+  U32 color;
   
   /* round to int */
   start_x = round_f32_to_s32(x);
@@ -38,6 +46,9 @@ EXTERNIZE void draw_rect(GameBackBuffer *back_buffer, F32 x, F32 y, F32 width, F
   if (end_x > back_buffer->width) end_x = back_buffer->width;
   if (end_y < 0) end_y = 0;
   if (end_y > back_buffer->height) end_y = back_buffer->height;
+  
+  /* pack floating point color representation to int - TODO: alpha channel */
+  color = ( (round_f32_to_u32(R * 255.0f) << 16) | (round_f32_to_u32(G * 255.0f) << 8) | (round_f32_to_u32(B * 255.0f) << 0) );
   
   /* start drawing */
   for (i = start_y; i < end_y; ++i) {
@@ -60,8 +71,8 @@ EXTERNIZE GAME_UPDATE_AND_RENDER_PROTOTYPE(game_update_and_render) {
   if (input->player1.right.pressed) { state->player_x += rect_move_speed; }
   
   /* Dirty clear background before drawing, TODO: a proper 'draw_background' */
-  draw_rect(back_buffer, 0.0f, 0.0f, CAST(F32) back_buffer->width, CAST(F32) back_buffer->height, 0x00FF00FF);
+  draw_rect(back_buffer, 0.0f, 0.0f, CAST(F32) back_buffer->width, CAST(F32) back_buffer->height, 1.0f, 0.0f, 1.0f);
   
   /* Player (rect) representation */
-  draw_rect(back_buffer, 50 + state->player_x, 50 + state->player_y, 80, 45, 0xFFF1F1F1);
+  draw_rect(back_buffer, 50 + state->player_x, 50 + state->player_y, 80, 45, 0.364705f, 0.464705f, 0.964705f);
 }
